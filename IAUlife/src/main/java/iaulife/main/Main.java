7029a1338dc1e845787290fcdbb21578bbb8c490
AIDARXAN <mainwinnertactics@gmail.com>
2018-11-14 21:37:16 +0600

    Shake class

--- IAUlife/src/main/java/iaulife/main/Main.java
+++ IAUlife/src/main/java/iaulife/main/Main.java
@@ -4,15 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Parent root = null;
+//        try {
+//            root = FXMLLoader.load(getClass().getResource("signIn.fxml"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/fxml/gui/news.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 338,51));
         primaryStage.show();
     }
 
