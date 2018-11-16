92ea04851c3918ba6b83960b94ce7eea4e6492ad
AIDARXAN <mainwinnertactics@gmail.com>
2018-11-16 22:17:25 +0600

    Rearranged path to the icons and images

--- IAUlife/src/main/java/iaulife/main/Main.java
+++ IAUlife/src/main/java/iaulife/main/Main.java
@@ -1,29 +1,30 @@
-package main.java.iaulife.main;
+package iaulife.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = null;
-//        try {
-//            root = FXMLLoader.load(getClass().getResource("signIn.fxml"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        AnchorPane root = FXMLLoader.load(getClass().getResource("/fxml/gui/news.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 338,51));
+        Parent panel = null;
+        try {
+            panel = FXMLLoader.load(getClass().getResource("main/java/iaulife/main/signIn.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        Parent panel = FXMLLoader.load(getClass().getResource("signIn.fxml"));
+
+        Scene scene = new Scene(panel, 338,51);
+        primaryStage.setTitle("International Ala-Too University Life");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
--- IAUlife/src/sample/AlertHelper.java
+++
@@ -1,7 +0,0 @@
-package sample;
-
-public class AlertHelper {
-    public void showAlert(){
-
-    }
-}
