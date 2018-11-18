bf6b32971e09a088fde13b221f26226c1a87e99a
AIDARXAN <mainwinnertactics@gmail.com>
2018-11-18 13:02:03 +0600

    Alert Helper class

--- IAUlife/src/main/java/iaulife/animation/AlertHelper.java
+++ IAUlife/src/main/java/iaulife/animation/AlertHelper.java
@@ -1,7 +1,17 @@
 package main.java.iaulife.animation;
 
-public class AlertHelper {
-    public void showAlert(){
+import javafx.scene.control.Alert;
+import javafx.stage.Modality;
+import javafx.stage.Window;
 
+public class AlertHelper {
+    public static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.showAndWait();
     }
 }
